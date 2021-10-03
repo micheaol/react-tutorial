422e59d0f7731e777c38adbecb0b217316d6bd16
micheaol <micheaol80@gmail.com>
2021-10-03 11:40:31 +0100

    Added feature

--- react-todo-app/src/index.js
+++ react-todo-app/src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+//Component files:
+import TodoContainer from "./components/TodoContainer";
 
-const element = <h1>Hello from React APP</h1>;
 
 
-ReactDOM.render(element, document.getElementById('root'));
+
+ReactDOM.render(<React.StrictMode><TodoContainer/></React.StrictMode>, document.getElementById('root'));
